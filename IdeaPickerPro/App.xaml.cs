52a97f6bb9d8105fab846a78a10ed5df5dafdfe6
Neebin Buswa <neebinnodin.buswa9132@fvtc.edu>
2025-11-28 00:41:03 -0600

    Beginning.

---
+++ IdeaPickerPro/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IdeaPickerPro;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ IdeaPickerPro/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace IdeaPickerPro;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
