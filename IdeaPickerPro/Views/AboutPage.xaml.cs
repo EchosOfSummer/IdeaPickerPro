f852fa6cb82773ff4978b95706c68efdc4f2c0de
Neebin Buswa <neebinnodin.buswa9132@fvtc.edu>
2025-12-14 18:36:04 -0600

    hmmm

--- IdeaPickerPro/Views/AboutPage.xaml.cs
+++ IdeaPickerPro/Views/AboutPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IdeaPickerPro.Models;
+//using Color = System.Drawing.Color;
+
 // using Color = System.Drawing.Color;
 
 namespace IdeaPickerPro.Views;
@@ -14,18 +16,25 @@ public partial class AboutPage : ContentPage
     {
         InitializeComponent();
         Glitch.StartGlitch(Layer1,Layer2,Layer3,Layer4);
-
-        Shell.Current.BackgroundColor = Color.FromArgb("#4a0080");
+        
+        Shell.SetBackgroundColor(this, Color.FromArgb("#8e2de2"));
+        // Shell.Current.BackgroundColor = Color.FromArgb("#4a0080");
         
         // System.Diagnostics.Debug.WriteLine($"Current Shell: {Shell.Current}");
         // System.Diagnostics.Debug.WriteLine($"Shell Background: {Shell.Current.BackgroundColor}");
         //
         // Shell.Current.BackgroundColor = Color.FromArgb("#ff006e");
-        // Shell.SetTabBarBackgroundColor(Shell.Current, Color.FromArgb("#4a00e0"));
+        Shell.SetTabBarBackgroundColor(Shell.Current, Colors.Green);
         
         // Glitch.StartNew();
         
         Title = "About";
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        Shell.SetBackgroundColor(this, Colors. Transparent);
     }
 }
 
