167e9ee79d4984e9dbf68fed085aa9eb0ca9c588
Neebin Buswa <neebinnodin.buswa9132@fvtc.edu>
2025-12-14 21:27:22 -0600

    done for now.

--- IdeaPickerPro/Platforms/iOS/AppDelegate.cs
+++ IdeaPickerPro/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,5 @@
-﻿using Foundation;
+﻿using UIKit;
+using Foundation;
 
 namespace IdeaPickerPro;
 
@@ -6,4 +7,22 @@ namespace IdeaPickerPro;
 public class AppDelegate : MauiUIApplicationDelegate
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    //
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+        // Nave bar style (page titles
+        UINavigationBar.Appearance.BackgroundColor = UIColor.Black;
+        UINavigationBar.Appearance.TintColor = UIColor.Cyan;
+        UINavigationBar.Appearance.TitleTextAttributes = new UIStringAttributes
+        {
+            ForegroundColor = UIColor.White
+        };
+
+        //Tab bar style
+        UITabBar.Appearance.BackgroundColor = UIColor.Black;
+        UITabBar.Appearance.TintColor = UIColor.Cyan;
+        UITabBar.Appearance.UnselectedItemTintColor = UIColor.LightGray;
+        
+        return base.FinishedLaunching(app, options);
+    }
 }
--- IdeaPickerPro/Views/AboutPage.xaml.cs
+++ IdeaPickerPro/Views/AboutPage.xaml.cs
@@ -17,24 +17,24 @@ public partial class AboutPage : ContentPage
         InitializeComponent();
         Glitch.StartGlitch(Layer1,Layer2,Layer3,Layer4);
         
-        Shell.SetBackgroundColor(this, Color.FromArgb("#8e2de2"));
+        // Shell.SetBackgroundColor(this, Color.FromArgb("#8e2de2"));
         // Shell.Current.BackgroundColor = Color.FromArgb("#4a0080");
         
         // System.Diagnostics.Debug.WriteLine($"Current Shell: {Shell.Current}");
         // System.Diagnostics.Debug.WriteLine($"Shell Background: {Shell.Current.BackgroundColor}");
         //
         // Shell.Current.BackgroundColor = Color.FromArgb("#ff006e");
-        Shell.SetTabBarBackgroundColor(Shell.Current, Colors.Green);
+        // Shell.SetTabBarBackgroundColor(Shell.Current, Colors.Green);
         
         // Glitch.StartNew();
         
         Title = "About";
     }
 
-    protected override void OnDisappearing()
-    {
-        base.OnDisappearing();
-        Shell.SetBackgroundColor(this, Colors. Transparent);
-    }
+    // protected override void OnDisappearing()
+    // {
+    //     base.OnDisappearing();
+    //     Shell.SetBackgroundColor(this, Colors. Transparent);
+    // }
 }
 
