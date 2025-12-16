685d57281adba924eb9f66b5b12e541a90f927da
Neebin Buswa <neebinnodin.buswa9132@fvtc.edu>
2025-12-16 00:38:00 -0600

    I'm done.. good enough..

--- IdeaPickerPro/Models/Glitch.cs
+++ IdeaPickerPro/Models/Glitch.cs
@@ -5,7 +5,7 @@ public static class Glitch
     public static async void StartGlitch(BoxView layer1, BoxView layer2, BoxView layer3, BoxView layer4)
     {
         var rand = new Random();
-
+        
         while (true)
         {
             var layers = new[] { layer1, layer2, layer3, layer4 };
@@ -17,7 +17,7 @@ public static class Glitch
                 await Task.Delay(rand.Next(50, 200));
                 layer.Opacity = 0;
             }
-
+        
             await Task.Delay(rand.Next(100, 800));
         }
     }
